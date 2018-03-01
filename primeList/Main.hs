1defe8bb8a301d3e3808016c0b404a87667101fc
mino2357 <mino2357@gmail.com>
2018-03-01 16:27:57 +0900

    add

--- primeList/Main.hs
+++ primeList/Main.hs
@@ -24,5 +24,42 @@ primeFactorizeList n
 primeFactorize :: Integer -> [(Integer, Integer)]
 primeFactorize n = [ tuple | tuple <- primeFactorizeList n, snd tuple /= 0 ]
 
+
+list = fmap primeFactorize [1..10]
+
+
+radWorker :: [(Integer, Integer)] -> Integer
+radWorker (x:xs)
+  | null xs = (fst x) * 1
+  | otherwise = (fst x) * radWorker xs
+
+radWorker2 :: [(Integer, Integer)] -> Integer
+radWorker2 (x:xs)
+  | null xs = (snd x) + 0
+  | otherwise = (snd x) + radWorker2 xs
+
+radWorker3 :: [(Integer, Integer)] -> Integer
+radWorker3 (x:xs)
+  | null xs = (snd x) * 1
+  | otherwise = (snd x) * radWorker3 xs
+
+radWorker4 :: [(Integer, Integer)] -> Integer
+radWorker4 (x:xs)
+  | null xs = (fst x) + 0
+  | otherwise = (fst x) + radWorker4 xs
+
+rad :: Integer -> [Integer]
+rad n = [n, (radWorker ( primeFactorize n))]
+
+rad' :: Integer -> [Integer]
+rad' n = [radWorker2 (primeFactorize (radWorker4 (primeFactorize n))), (radWorker2 (primeFactorize n))]
+
+cut :: Show a => [a] -> String
+cut (x:xs) = show x ++ " " ++ (show ( last xs))
+
+
 main = do
-  mapM_ print $ fmap primeFactorize [1..10000]
+  mapM_ putStrLn $ fmap cut (fmap rad' [2..10000])
+
+
+
