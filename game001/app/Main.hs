7ac070592f2ec8ff2504eb744a6a67fd21c3ed52
mino2357 <mino2357@gmail.com>
2018-04-02 04:54:33 +0900

    add repa

--- game001/app/Main.hs
+++ game001/app/Main.hs
@@ -4,11 +4,16 @@ import Lib
 import Leibniz
 import Control.Monad
 import Data.Char
-import Data.Array.Repa
+import Data.Array.Repa as R
 
+type Vector1d = R.Array R.U R.DIM1 Double
 
---type Scaler t = R.Array t R.Dim2 Double
+u :: Vector1d
+u = R.fromListUnboxed (ix1 10) [0..9]
 
+v :: Vector1d
+v = R.fromListUnboxed (ix1 10) $ take 10 [0,0..]
 
 main = do
-  leibniz 10
+  print $ (u +^ v) ! (Z:.4)
+  --leibniz 10
