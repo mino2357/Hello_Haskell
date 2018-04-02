187b20b80431f251a94261e4e7e953b05f3f8e82
mino2357 <mino2357@gmail.com>
2018-04-02 22:21:06 +0900

    add

--- game001/app/Main.hs
+++ game001/app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeOperators #-}
 module Main where
 
 import Lib
@@ -6,14 +7,22 @@ import Control.Monad
 import Data.Char
 import Data.Array.Repa as R
 
-type Vector1d = R.Array R.U R.DIM1 Double
+type Vector1d = R.Array R.U (Z:.Int) Double
+type Vector2d = R.Array R.U (Z:.Int:.Int) Double
+
+dim :: Int
+dim = 10
 
 u :: Vector1d
-u = R.fromListUnboxed (ix1 10) [0..9]
+u = R.fromListUnboxed (Z:.dim) $ take dim [0,1..]
+
+u2 :: Vector2d
+u2 = R.fromListUnboxed (Z:.dim:.dim) $ take (dim*dim) [0..]
 
 v :: Vector1d
-v = R.fromListUnboxed (ix1 10) $ take 10 [0,0..]
+v = R.fromListUnboxed (Z:.dim) $ take 10 [0,0..]
 
 main = do
   print $ (u +^ v) ! (Z:.4)
+  print $ u2 
   --leibniz 10
